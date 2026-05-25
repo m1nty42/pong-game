d4294604dd7df54c72e487ab645fb95a2cc7c32e
m1nty42 <jmbxts4@gmail.com>
2026-05-25 16:07:56 -0400

    I think it's done, made it easier for the player to win

--- scenes/cpu.gd
+++ scenes/cpu.gd
@@ -6,6 +6,7 @@ var move_by : int
 var win_height : int
 var p_height : int
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	win_height = get_viewport_rect().size.y
@@ -16,14 +17,17 @@ func _process(delta: float) -> void:
 	#move paddle towards ball
 	ball_pos = $"../Ball".position
 	dist = position.y - ball_pos.y
+
 	
-	if abs(dist) > get_parent().PADDLE_SPEED * delta:
-		move_by = get_parent().PADDLE_SPEED * delta * (dist / abs(dist))
+	if abs(dist) > get_parent().AI_PADDLE_SPEED * delta:
+		move_by = get_parent().AI_PADDLE_SPEED * delta * (dist / abs(dist))
 	else:
-		move_by = dist
-	
+		move_by = dist 
+		
 	position.y -= move_by
 	
 	#limit paddle movement to window
 	position.y = clamp(position.y, p_height / 2, win_height - p_height / 2)
+	
+	
 	
--- scenes/main.gd
+++ scenes/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var score := [0, 0]# 0:Player, 1: CPU
 const PADDLE_SPEED : int = 500
+const AI_PADDLE_SPEED : int = 300
 
 
 func _on_ball_timer_timeout() -> void:
